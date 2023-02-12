20be9f4c3bdffaf3e1eb108351761a01fc2f2b63
erik4ka <yarik19092008@gmail.com>
2023-02-12 14:18:52 +0200

    les3

--- les5-6/les_5_cw.py
+++ les5-6/les_5_cw.py
@@ -44,13 +44,40 @@ class Archer():
  
  
 class SpellSword(Warrior, Mage):
-    def __init__(self):
-        super().__init__("Spellsword", 20)
+    def __init__(self, name="Spellsword", points=20):
+        super().__init__(name, points)
  
     def enchant_sword(self):
         print("Sword was successfully enchant on fire")
  
  
-ss = SpellSword()
+class Pathfinder(Warrior, Archer):
+    def __init__(self, name="Pathfinder", points=10):
+        super().__init__(name, points)
+ 
+    def toss_dagger(self):
+        print("Dagger toss")
+ 
+ 
+class ElementalArcher(Mage, Archer):
+    def __init__(self, name="ElementalArcher", points=15):
+        super().__init__(name, points)
+ 
+    def elemental_arrow(self):
+        print("Element arrow")
+ 
+ 
+class Hero(ElementalArcher, Pathfinder, SpellSword):
+    def __init__(self, name="Hero", points=50):
+        super().__init__(name, points)
+ 
+    def weapon_world(self):
+        print("weapon_world")
+ 
+ 
+ss = Hero()
 ss.rage()
-print(ss.name)
+ss.fireball()
+ss.enchant_sword()
+ss.print(ss.name)
+ 
