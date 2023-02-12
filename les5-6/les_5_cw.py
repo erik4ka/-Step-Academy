2c57756d39d4caec69f549a588556779bb037925
erik4ka <yarik19092008@gmail.com>
2023-02-12 14:09:47 +0200

    cw2

--- les5-6/les_5_cw.py
+++ les5-6/les_5_cw.py
@@ -2,25 +2,55 @@ class Basic_Class:
     def __init__(self, name, points):
         self.name = name
         self.points = points
-
+ 
     def teleport(self):
         print("Teleporting")
-
-
-class Warrior(Basic_Class):
-    # sword_attack
-    # rage
-
-
-class Mage(Basic_Class):
+ 
+ 
+class Warrior():
+    def __init__(self, name, points):
+        self.name = name
+        self.points = points
+ 
+    def sword_attack(self):
+        print("Sword Attack")
+ 
+    def rage(self):
+        print("Rage")
+ 
+ 
+class Mage():
+    def __init__(self, name, points):
+        self.name = name
+        self.points = points
+ 
+    def fireball(self):
+        print("Fireball")
+ 
+    def icewall(self):
+        print("Icewall")
+ 
+ 
+class Archer():
+    def __init__(self, name, points):
+        self.name = name
+        self.points = points
+ 
+    def trap(self):
+        print("Trap")
+ 
+    def invisibility(self):
+        print("Invisibility")
+ 
+ 
+class SpellSword(Warrior, Mage):
     def __init__(self):
-        super("Mage", 10)
-
-    # fireball
-    # icewall
-
-
-class Archer(Basic_Class):
-
-    # trap
-    # invisible
+        super().__init__("Spellsword", 20)
+ 
+    def enchant_sword(self):
+        print("Sword was successfully enchant on fire")
+ 
+ 
+ss = SpellSword()
+ss.rage()
+print(ss.name)
